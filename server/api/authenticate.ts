31d37226c0b5ae9fa0d1cc6c1602523ce6b34846
Sarav <sarav.majestic@gmail.com>
2022-07-10 11:39:43 +0530

    chore: remove extra code

--- server/api/authenticate.ts
+++ server/api/authenticate.ts
@@ -4,6 +4,5 @@ export default function handler(request: Request, response: Response) {
   response.status(200).json({
     body: request.body,
     query: request.query,
-    cookies: request.cookies,
   });
 }
